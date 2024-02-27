c02361424776d068a33318a2c4dd765d811c91d0
Marcos Vinícius Correia da Silva <marvinc.silva@gmail.com>
2024-02-27 15:23:57 +0100

    chore: encode the service for download files from s3

--- tests/services/test_s3_service.py
+++ tests/services/test_s3_service.py
@@ -1,14 +1,63 @@
+import os
 from random import randint
 from unittest import TestCase
-from unittest.mock import Mock
+from unittest.mock import patch, MagicMock, Mock
 
 from fullstack_test.services.s3_service import S3Service
 
 class TestS3Service(TestCase):
-    def test_that_it_returns_the_input(self):
+    @patch("boto3.client")
+    def test_download_success(self, mock_boto3_client):
+        # Mock the S3 client
+        mock_s3 = MagicMock()
+        mock_boto3_client.return_value = mock_s3
+
+        # Configure the mock_s3 to return a simulated PDF content
+        simulated_pdf_content = b"Simulated PDF Content"
+        mock_s3.get_object.return_value = {'Body': MagicMock(read=MagicMock(return_value=simulated_pdf_content))}
+
+        bucket_name = "test"
+        aws_access_key="access"
+        aws_secret_key="secret"
+
+        # Create an instance of S3Service
+        s3_service = S3Service(bucket_name, aws_access_key, aws_secret_key)
+
+        # Call the download_pdf method
         invoice_id = randint(1, 100)
-        s3_service = S3Service()
+        pdf_content = s3_service.download(invoice_id)
 
-        actual = s3_service.download(invoice_id)
+        # Assertions
+        mock_boto3_client.assert_called_with('s3', aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_key)
+        mock_s3.get_object.assert_called_with(Bucket=bucket_name, Key=invoice_id)
+        self.assertEqual(pdf_content, simulated_pdf_content)
 
-        self.assertEqual(f"{invoice_id}_access_secret", actual)
+    @patch("boto3.client")
+    def test_download_failure(self, mock_boto3_client):
+        # Mock the S3 client
+        mock_s3 = MagicMock()
+        mock_boto3_client.return_value = mock_s3
+
+        # Configure the mock_s3 to thrown
+        mock_s3.get_object.side_effect = Exception("Simulated S3 Error")
+        # simulated_pdf_content = b"Simulated PDF Content"
+        # mock_s3.get_object.return_value = {'Body': MagicMock(read=MagicMock(return_value=simulated_pdf_content))}
+
+        bucket_name = "test"
+        aws_access_key="access"
+        aws_secret_key="secret"
+
+        # Create an instance of S3Service
+        s3_service = S3Service(bucket_name, aws_access_key, aws_secret_key)
+
+        invoice_id = randint(1, 100)
+
+        with self.assertRaises(ValueError) as context:
+            s3_service.download(invoice_id)
+
+        # Call the download_pdf method
+
+        # Assertions
+        mock_boto3_client.assert_called_with('s3', aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_key)
+        mock_s3.get_object.assert_called_with(Bucket=bucket_name, Key=invoice_id)
+        self.assertEqual(str(context.exception), "Failed to retrieve file from S3")

b0d5d335b9a449c1fbd34f0630c703c81628ab75
Fabio William Conceição <messhias@gmail.com>
2024-03-19 19:46:36 +0000

    show invoice pdf

--- fullstack_test/api_routes.py
+++ fullstack_test/api_routes.py
@@ -8,10 +8,10 @@ app = FastAPI()
 # Fixing cors issues
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # since it's a test doesn't matter match specific domains
+    allow_origins=["*"],  # since it's a test doesn't need to match specific domains
     allow_credentials=True,
-    allow_methods=["GET", "POST", "DELETE", "PATCH"],  # let do anything for
-    allow_headers=["*"],  # Allow receive any header.
+    allow_methods=["GET", "POST", "DELETE", "PATCH"],  # let it do anything for
+    allow_headers=["*"],  # Allow any header.
 )
 
 invoice_api = InvoiceApi()
